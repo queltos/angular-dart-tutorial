b4338791c30c995cea4947f035f7c765d0c4c3a4
Daniel <queltos@gmail.com>
2017-03-02 19:45:17 +0100

    Editing Heroes

--- lib/hero_service.dart
+++ lib/hero_service.dart
@@ -1,17 +1,75 @@
 import 'dart:async';
+import 'dart:convert';
+
 import 'package:angular2/core.dart';
+import 'package:http/http.dart';
 
 import 'hero.dart';
-import 'mock_heroes.dart';
 
 @Injectable()
 class HeroService {
-  Future<List<Hero>> getHeroes() async => mockHeroes;
+  static const _heroesUrl = 'api/heroes';
 
-  Future<List<Hero>> getHeroesSlowly() {
-    return new Future.delayed(const Duration(seconds: 2), getHeroes);
+  final Client _http;
+  final _headers = {'Content-Type': 'application/json'};
+
+  HeroService(this._http);
+
+  Future<List<Hero>> getHeroes() async {
+    try {
+      final response = await _http.get(_heroesUrl);
+      final heroes = _extractData(response)
+          .map((value) => new Hero.fromJson(value))
+          .toList();
+      return heroes;
+    } catch(e) {
+      throw _handleError(e);
+    }
   }
 
-  Future<Hero> getHero(int id) async =>
-      (await getHeroes()).firstWhere((hero) => hero.id == id);
+  dynamic _extractData(Response resp) => JSON.decode(resp.body)['data'];
+
+  Exception _handleError(dynamic e) {
+    print(e);
+    return new Exception('Server error; cause: $e');
+  }
+
+  Future<Hero> getHero(int id) async {
+    try {
+      final response = await _http.get('$_heroesUrl/$id');
+      return new Hero.fromJson(_extractData(response));
+    } catch(e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Hero> update(Hero hero) async {
+    try {
+      final url = '$_heroesUrl/${hero.id}';
+      final response =
+          await _http.put(url, headers: _headers, body: JSON.encode(hero));
+      return new Hero.fromJson(_extractData(response));
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Hero> create(String hero) async {
+    try {
+      final response = await _http.post(_heroesUrl,
+          headers: _headers, body: JSON.encode({'name': hero}));
+      return new Hero.fromJson(_extractData(response));
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Null> delete(int id) async {
+    try {
+      final String url = '$_heroesUrl/$id';
+      final response = await _http.delete(url, headers: _headers);
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,10 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/platform/browser.dart';
-
 import 'package:angular2/platform/common.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
+
+import 'package:angular_quickstart/in_memory_data_service.dart';
 import 'package:angular_quickstart/app_component.dart';
 
 void main() {
@@ -9,5 +12,8 @@ void main() {
   // http://stackoverflow.com/questions/41965060/dart-angular-2-pub-serve-404-on-page-reload
   // and supposed to fix the reloading issue for deep urls. Doesn't work though
   bootstrap(AppComponent, [
+    provide(Client, useClass: InMemoryDataService),
+//    for production use:
+//    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: []),
     provide(LocationStrategy, useClass: HashLocationStrategy)]);
 }
